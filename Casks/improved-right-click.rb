970659125025a3a8e08a3e4231d1f13860fd1d6b
Astrak00 <edualanav@gmail.com>
2026-05-18 00:29:57 +0200

    Add improved-right-click cask v1.0.0

---
+++ Casks/improved-right-click.rb
@@ -0,0 +1,29 @@
+cask "improved-right-click" do
+  version "1.0.0"
+  # Run `shasum -a 256 ImprovedRightClick-<version>.dmg` and paste here:
+  sha256 "657703524a2c22563b006e092ed1bd94841db59501204faeeb89915ab32ba457"
+
+  # Replace with the actual GitHub release URL after you publish it:
+  url "https://github.com/Astrak00/ImprovedRightClick/releases/download/v#{version}/ImprovedRightClick-#{version}.dmg",
+      verified: "github.com/Astrak00/ImprovedRightClick/"
+
+  name "Improved Right Click"
+  desc "Bring the right click > create new file to Finder"
+  homepage "https://github.com/Astrak00/ImprovedRightClick"
+
+  depends_on macos: ">= :ventura"
+
+  app "ImprovedRightClick.app"
+
+  postflight do
+    system_command "/usr/bin/open",
+                   args: ["-a", "ImprovedRightClick"]
+  end
+
+  uninstall quit: "com.improvedrightclick.app"
+
+  zap trash: [
+    "~/Library/Group Containers/group.com.improvedrightclick.app",
+    "~/Library/Preferences/com.improvedrightclick.app.plist",
+  ]
+end
